d71a360b0abb72a1de00659ee1370709ae5fe9a2
BasantJoshi10 <basantjoshi792@gmail.com>
2026-05-16 09:34:31 +0530

    updated code with navbar and card

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import style from "../styles/navbar-module.css"
 
 const navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+        <nav className="navbar">
+        <div className="logo">MyProject</div>
+
+        <ul className="nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Services</a></li>
+            <li><a href="/">Contact</a></li>
+        </ul>
+
+        <button className="login-btn">Login</button>
+        </nav>
+  );
+};
+
+
 
 export default navbar

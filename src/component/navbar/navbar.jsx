7266fb2d148e2a731093ca00afc3eca62eac3449
BasantJoshi10 <basantjoshi792@gmail.com>
2026-05-17 13:18:07 +0530

    navbar fixed

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 import navbar from '../navbar/navbar-module.css'
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    const handleLogin = () => {
+    navigate('/login');
+  };
   return (
     <nav className="navbar">
-        <div className="logo">MyProject</div>
+        <div className="logo">Joshi'sNews</div>
         <ul className="nav-links">
             <li><a href="/">Home</a></li>
             <li><a href="/">About</a></li>
@@ -12,7 +17,7 @@ const Navbar = () => {
             <li><a href="/">Contact</a></li>
         </ul>
 
-        <button className="login-btn">Login</button>
+        <button className="login-btn1" onClick={handleLogin}>Login</button>
       </nav>
   )
 }
